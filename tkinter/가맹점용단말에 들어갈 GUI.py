54c17f2520858766d0e2e4e73fc4a45d0ce5e391
yandusty <49185078+yandusty@users.noreply.github.com>
2019-11-04 23:28:19 +0900

    qrcode작성까지 포함한 새로운 버젼

--- tkinter/가맹점용단말에 들어갈 GUI.py
+++ tkinter/가맹점용단말에 들어갈 GUI.py
@@ -13,12 +13,12 @@ def click(key):
         entry.insert(END,key)
 
 
-'''def create(key):
+def create(key):
     if key == 'create qrcode':
         photo = PhotoImage(file = "test.png")
         w = Label(window, image = photo)
         w.photo = photo
-        w.grid(column = 5,row =2,rowspan=4)'''
+        w.grid(column = 5,row =2,rowspan=4)
 
 
 window = Tk()
@@ -34,14 +34,14 @@ canvas_3 = Canvas(window, bg = 'white', width = 200, height = 60)
 canvas_4 = Canvas(window, bg = 'white', width = 200, height = 180)
 entry = Entry(window, bg = 'white', width = 20)
 canvas_6 = Canvas(window, bg = 'white', width = 200, height = 240)
-#check = Button(window, text = 'create qrcode',width = 20, command = lambda y='create qrcode': create(y))
+check = Button(window, text = 'create qrcode',width = 20, command = lambda y='create qrcode': create(y))
 
 canvas_2.grid(column=1, row=1)
 canvas_3.grid(column=1, row=2)
 canvas_4.grid(column=1, row=3, rowspan = 3)
 canvas_6.grid(column=5, row=2, rowspan = 4)
 entry.grid(column=2, row=1, columnspan = 2)
-#check.grid(column=5, row = 1)
+check.grid(column=5, row = 1)
 
 i = 0
 a = 0
