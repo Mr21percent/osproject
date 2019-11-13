a10b622e05601c87273c30d1f6cad9fdb1bc1c8f
may <maychoi410@gmail.com>
2019-11-14 00:41:17 +0900

    .

--- flask_proj/test.py
+++ flask_proj/test.py
@@ -2,9 +2,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/hello/')
 def hello_world():
     return 'Hello World!'
 
+@app.route('/profile/<username>')
+def hello_world(username):
+    return 'profile: ' + username
+
+host_addr = "0.0.0.0"
+port_num = "8080"
+
 if __name__=='__main__':
-    app.run()
+	with app.test_request_context():
+		print(url_for('hello'))
+		print(url_for('get_profile', username='flash'))
+	# app.run(host=host_addr, port=port_num)
