ad18efdb46668e476b193ffcb43aab213bdefc29
Mr21percent <woneunglee@naver.com>
2019-11-22 16:22:00 +0900

    a

--- made/almost/read_qr_and_give_store_price.py
+++ made/almost/read_qr_and_give_store_price.py
@@ -32,11 +32,58 @@ while True:
  
         # find the barcodes in the frame and decode each of the barcodes
         barcodes = pyzbar.decode(frame)
+        for barcode in barcodes:
+                # extract the bounding box location of the barcode and draw
+                # the bounding box surrounding the barcode on the image
+                (x, y, w, h) = barcode.rect
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+                # the barcode data is a bytes object so if we want to draw it
+                # on our output image we need to convert it to a string first
+                barcodeData = barcode.data.decode("utf-8")
+                barcodeType = barcode.type
+
+                # draw the barcode data and barcode type on the image
+                text = "{} ({})".format(barcodeData, barcodeType)
+                cv2.putText(frame, text, (x, y - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+
+                # if the barcode text is currently not in our CSV file, write
+                # the timestamp + barcode to disk and update the set
+                if barcodeData not in found:
+                        csv.write("{},{}\n".format(datetime.datetime.now(),
+                                barcodeData))
+                        csv.flush()
+                        found.add(barcodeData)
+        # show the output frame
+        cv2.imshow("Barcode Scanner", frame)
+        key = cv2.waitKey(1) & 0xFF
         if barcodes != []:
-            break
+                cv2.destroyWindow("Barcode Scanner")
+                break
 # barcodes[0][0] code for store and price
 
 price,store=barcodes[0][0].split('##')
 print(price, store)
 
-#need to send price and store to server
+
+
+global i_have_card
+i_have_card=[['이마트 KB카드', '<결제코드1>'],['삼성 S클래스 카드', '<켤제코드2>']]
+def plus_card(name,signal):
+    for i in range(len(i_have_card)):
+        if name==i_have_card[i][0]:
+            return 0
+    list=[]
+    list.append(name)
+    list.append(signal)
+    i_have_card.append(list)
+
+def rm_card(name):
+    i = 0
+    while i < len(i_have_card) :
+        if(i_have_card[i][0] == name) :
+            i_have_card.pop(i)
+            i -= 1
+        i += 1
+
