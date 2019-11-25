cfe9024a51268d1d1f1c83bacfd9efab47acd4e7
may <maychoi410@gmail.com>
2019-11-26 01:24:55 +0900

    .

--- virtual_flask/flaskapp/__init__.py
+++ virtual_flask/flaskapp/__init__.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request
-from firebase import firebase
+from flask import Flask, request, jsonify
+#from firebase import firebase
 
 app = Flask(__name__)
 app.debug = True
@@ -7,20 +7,17 @@ app.debug = True
 @app.route("/")
 def helloworld():
 	return "Hello Flask World!"
+
 '''
 @app.before_request
 def before_request():
 	print("before request!!")
 '''
 
-@app.route('/fileUpload', methods=['GET', 'POST'])
-def upload_file():
-	if request.method == 'POST':
-		f = request.files['file']
-		f.save(secure_filename(f.filename))
-		return 'uploads 디렉토리 -> 파일 업로드 성공!'
-
 @app.route('/index', methods=['GET'])
 def index():
 	testData = 'testData array'
 	return render_template('')
+
+
+@app.route('/test'
