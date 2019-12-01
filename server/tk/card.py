4bf8b2343bbd5d504c0934048a3c1625f43a679b
may <maychoi410@gmail.com>
2019-12-01 21:27:18 +0900

    crawl

--- server/tk/card.py
+++ server/tk/card.py
@@ -52,7 +52,7 @@ def mod_card(signal):
 	rightF.pack(side=RIGHT, ipadx="3m", ipady="1m", padx="3m", pady="2m")
 
 	def b1Click():
-		nonlocal signal
+		global signal
 		index = listbox.curselection()[0]
 		card_name = data_origin[index][0]
 		card_in_data = False
